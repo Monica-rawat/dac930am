0b0832b1e6786ebd669fce67e158f1a6888afd2b
monica-rawat <monicarawat@gmail.com>
2023-06-03 12:41:50 +0530

    #modified

--- url.js
+++ url.js
@@ -1,2 +1,2 @@
 //module.exports = `mongodb://localhost:27017`
-npxmodule.exports = `mongodb+srv://admin:admin@cluster0.1t4xcov.mongodb.net`
+module.exports = `mongodb+srv://admin:admin@cluster0.1t4xcov.mongodb.net`
